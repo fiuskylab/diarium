06375dbf4b8791a5cb02003a49ddd9f8c8ef0546
Rafael Breno <rafael_breno@outlook.com>
2021-08-01 16:10:05 -0300

    feat: Update comments

--- output_file.go
+++ output_file.go
@@ -11,12 +11,15 @@ type file struct {
 	w *os.File
 }
 
+// output writes given log into file
 func (f *file) output(i interface{}) error {
 	b, _ := json.Marshal(i)
 	_, err := fmt.Fprintln(f.w, string(b))
 	return err
 }
 
+// newTerminal add the output option to print
+// logs into given file
 func newFile(w *os.File) Output {
 	return Output(&file{
 		w: w,
