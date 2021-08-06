b9ef594f8350850dff18a41e9e5efcd54a7a3d3d
Rafael Breno <rafael_breno@outlook.com>
2021-08-05 22:12:30 -0300

    feat: Increase test_test scope

--- test_test.go
+++ test_test.go
@@ -1,28 +1,51 @@
 package diarium
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+type TestType int
+
+const (
+	Equal TestType = iota
+	Nil
+	Regex
+)
+
 type testCases struct {
-	name string
-	want interface{}
-	got  interface{}
+	name      string
+	want      interface{}
+	got       interface{}
+	regexRule *regexp.Regexp
+	testType  TestType
 }
 
 var testInterface = struct {
-	name string `json:name`
+	Name string `json:"name"`
 }{
-	name: "Foo",
+	Name: "Foo",
 }
 
 func runTests(t *testing.T, tts []testCases) {
 	for _, tt := range tts {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			assert.Equal(t, tt.want, tt.got)
+			switch tt.testType {
+			case Equal:
+				assert.Equal(t, tt.want, tt.got)
+				break
+			case Regex:
+				assert.Regexp(t, tt.regexRule, tt.got)
+				break
+			case Nil:
+				assert.Nil(t, tt.got)
+				break
+			default:
+				break
+			}
 		})
 	}
 }
