7664d0c91dfe937a34c302fca073498504f554ef
Rafael Breno <rafael_breno@outlook.com>
2021-08-05 22:55:32 -0300

    feat: Update Log function to fit LogBody

--- client.go
+++ client.go
@@ -1,6 +1,8 @@
 package diarium
 
-// Client
+// Client is the main part of the logging
+// tool. It's responsible for every public info and action
+// related to it.
 type Client struct {
 	cfg *Config
 }
@@ -23,9 +25,9 @@ func NewClient(cfg *Config) *Client {
 
 // Log prints a given interface through all
 // configured outputs
-func (c *Client) Log(i LogBody) error {
+func (c *Client) Log(lvl, message string, extra map[string]string) error {
 	for _, o := range c.cfg.Outputs {
-		err := o.output(i)
+		err := o.output(newLogBody(lvl, message, extra))
 		if err != nil {
 			return err
 		}
